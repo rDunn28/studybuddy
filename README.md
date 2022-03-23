2e5c561c86126fc00a1078c4032974017ca16875
rDunn28 <r.dunn2474@gmail.com>
2022-03-23 00:31:03 -0400

    Added note taking functionality

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Study Buddy - Cobra CLI Tutorial
 
 Project based learning, following  "How to build an interactive CLI app with Cobra and Promptui | Go tutorial"  on Youtube.
+
+https://divrhino.com/articles/build-interactive-cli-app-with-go-cobra-promptui/
+
--- cmd/new.go
+++ cmd/new.go
@@ -16,22 +16,114 @@ limitations under the License.
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
-	"github.com/spf13/cobra"
 	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
+	"github.com/studybuddy/data"
 )
 
 // newCmd represents the new command
 var newCmd = &cobra.Command{
 	Use:   "new",
 	Short: "Creates a new studybuddy note",
-	Long: `A longer description that describes that this creates a new studybuddy note`,
+	Long:  `A longer description that describes that this creates a new studybuddy note`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("new called")
+		createNewNote()
 	},
+}
+
+type promptContent struct {
+	errorMsg string
+	label    string
 }
 
 func init() {
 	noteCmd.AddCommand(newCmd)
+}
+
+func promptGetInput(pc promptContent) string {
+	validate := func(input string) error {
+		if len(input) <= 0 {
+			return errors.New(pc.errorMsg)
+		}
+		return nil
+	}
+
+	templates := &promptui.PromptTemplates{
+		Prompt:  "{{ . }} ",
+		Valid:   "{{ . | green }} ",
+		Invalid: "{{ . | red }} ",
+		Success: "{{ . | bold }} ",
+	}
+
+	prompt := promptui.Prompt{
+		Label:     pc.label,
+		Templates: templates,
+		Validate:  validate,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Input: %s\n", result)
+
+	return result
+}
+
+func promptGetSelect(pc promptContent) string {
+	items := []string{"animal", "food", "person", "object"}
+	index := -1
+	var result string
+	var err error
+
+	for index < 0 {
+		prompt := promptui.SelectWithAdd{
+			Label:    pc.label,
+			Items:    items,
+			AddLabel: "Other",
+		}
+
+		index, result, err = prompt.Run()
+
+		if index == -1 {
+			items = append(items, result)
+		}
+	}
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Input: %s\n", result)
+
+	return result
+}
+
+func createNewNote() {
+	wordPromptContent := promptContent{
+		"Please provide a word.",
+		"What word would you like to make a note of?",
+	}
+	word := promptGetInput(wordPromptContent)
+
+	definitionPromptContent := promptContent{
+		"Please provide a definition.",
+		fmt.Sprintf("What is the definition of the %s?", word),
+	}
+	definition := promptGetInput(definitionPromptContent)
+
+	categoryPromptContent := promptContent{
+		"Please provide a category.",
+		fmt.Sprintf("What category does %s belong to?", word),
+	}
+	category := promptGetSelect(categoryPromptContent)
+
+	data.InsertNote(word, definition, category)
 }
